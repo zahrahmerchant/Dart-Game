66a129029880b464b128d5d628596d9c3df7eb63
zahrahmerchant <67435710+zahrahmerchant@users.noreply.github.com>
2020-08-06 20:24:39 +0530

    Add files via upload

---
+++ Scripts/CameraSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera fullViewCamera;
+    public Camera dartViewCamera;
+
+    public void ShowOverheadView()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fullViewCamera.enabled = false;
+            dartViewCamera.enabled = true;
+        }
+        else
+        {
+            fullViewCamera.enabled = true;
+        }
+        
+    }
+}
